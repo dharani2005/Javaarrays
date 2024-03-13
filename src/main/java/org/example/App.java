a3badab9d60070d229f9e9b470442def10f14859
dharani2005 <58557415+dharani2005@users.noreply.github.com>
2024-03-13 15:04:49 +0100

    Adding the string array

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,22 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+        String[] newNames = {"sree dharani", "sreehan reddy", "Avyukth reddy"};
+        NameRepository.setNames(newNames);
+        System.out.println("Size of names array: " + NameRepository.getSize());
+        System.out.println("\nAll names:");
+        String[] allNames = NameRepository.findAll();
+        for (String name : allNames)
+        {
+            System.out.println(name);
+        }
+        NameRepository.clear();
+        System.out.println("\nSize of names array after clearing: " + NameRepository.getSize());
     }
 }
---
+++ src/main/java/org/example/ArrayDemo.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String[] names = new String[3];
+        names[0] = "sree";
+        names[1] = " ";
+        names[2] = "dharani";
+        System.out.print(names[0]+names[1]+names[2]);
+
+
+    }
+}
