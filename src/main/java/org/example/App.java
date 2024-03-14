628095e48e7a41fdb7b8a47a939d59380f37575b
dharani2005 <58557415+dharani2005@users.noreply.github.com>
2024-03-14 10:52:15 +0100

    workshop part2 is added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,25 +1,33 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
+import java.util.Scanner;
 
-
+public class App {
+    public static void main(String[] args) {
         String[] newNames = {"sree dharani", "sreehan reddy", "Avyukth reddy"};
         NameRepository.setNames(newNames);
         System.out.println("Size of names array: " + NameRepository.getSize());
         System.out.println("\nAll names:");
         String[] allNames = NameRepository.findAll();
-        for (String name : allNames)
-        {
+        for (String name : allNames) {
             System.out.println(name);
         }
+        String foundname = NameRepository.find("Avyukth reddy");
+        if (foundname != null) {
+            System.out.println("\nFound name: " + foundname);
+        } else {
+            System.out.println("\nName not found.");
+        }
+        boolean addednewname = NameRepository.add("rama mohan reddy");
+        System.out.println("new name is added  " + addednewname);
+
+
         NameRepository.clear();
         System.out.println("\nSize of names array after clearing: " + NameRepository.getSize());
     }
 }
+
+
+
+
+
--- src/main/java/org/example/NameRepository.java
+++ src/main/java/org/example/NameRepository.java
@@ -1,23 +1,48 @@
 package org.example;
 
 import java.util.Arrays;
-public class NameRepository
-{
-private static String[] names = new String[0];
-public static int getSize()
-{
+
+public class NameRepository {
+    private static String[] names = new String[0];
+
+    public static int getSize() {
         return names.length;
-}
-public static void setNames(String[] newNames)
-{
+    }
+
+    public static void setNames(String[] newNames) {
         names = Arrays.copyOf(newNames, newNames.length);
-}
-public static void clear()
-{
+    }
+
+    public static void clear() {
         names = new String[0];
-}
-public static String[] findAll()
-{
+    }
+
+    public static String[] findAll() {
         return Arrays.copyOf(names, names.length);
+    }
+
+    /* finding the fullname*/
+    public static String find(final String fullname) {
+        for (String element : names) {
+            if (element.equalsIgnoreCase(fullname)) {
+                return element;
+
+            }
+        }
+        return null;
+    }
+
+    public static boolean add(final String fullname) {
+        String foundfullname = find(fullname);
+        if (foundfullname != null) {
+            return false;
+        }
+        String[] newNames = Arrays.copyOf(names, names.length + 1);
+        newNames[newNames.length - 1] = fullname;
+        names = newNames;
+        return true;
+    }
 }
-}
+
+
+
